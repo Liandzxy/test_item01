7378944e93acbe398ac849d4d6db4123809326ff
Liandzxy <103056777+Liandzxy@users.noreply.github.com>
2022-10-04 10:41:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 # 导包
 import time
 import unittest
-import HtmlTestRunner
+from HtmlTestRunner import HTMLTestRunner
 
 
 suite = unittest.defaultTestLoader.discover("./case", pattern="test_login.py")
 
-file_path = "./report/{}.html".format(time.strftime("%Y_%m_%d %H_%M_%S"))
+file_path = "./report/{}.html".format(time.strftime("%Y_%m_%d_%H_%M_%S"))
 
-with open(file_path, "wb") as f:
-    HtmlTestRunner(stream=f).run(suite)
+with open(file_path, "w") as f:
+    HTMLTestRunner(stream=f).run(suite)
